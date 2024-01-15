947e145462f1b28e86186db5e724ecbb76f73c6b
LawrenceLee <>
2024-01-15 13:30:27 -0800

    fixed typo and added exceptions for invalid inputs.

--- day2_area_circle.py
+++ day2_area_circle.py
@@ -1,8 +1,19 @@
 import math
 
-def calc_area_cirlce(radius):
+def calc_area_circle(radius):
+
+  try:
+    radius = float(radius)
+  except ValueError:
+    raise ValueError("radius must be a number")
+
+  # mathematically, radius cannot be negative
+  # a negative circle area doesn't make sense
+  if radius < 0:
+    raise ValueError("radius cannot be negative")
+
   return (math.pi) * radius**2
 
-r = float(input("enter raidus: "))
+r = input("enter raidus: ")
 a = calc_area_circle(r)
 print(f"circle with radius {r} has area {a}")
