73a431635d49b09e2eda131aae05579829556e84
LawrenceLee <>
2024-01-14 21:40:14 -0800

    fixed but with print statments, fixed priority queue function.

--- day5_min_max.py
+++ day5_min_max.py
@@ -30,11 +30,13 @@ def min_max_builtin(input_list):
 
 # using min and max heaps to keep track of the min and max
 def min_max_priority_queues(input_list):
+    import heapq 
     max_heap = []
     min_heap = []
     for curr_num in input_list:
         heapq.heappush(max_heap, -curr_num)
         heapq.heappush(min_heap, curr_num)
+    return min_heap[0], -max_heap[0]
 
 # I am assuming the input array is valid and only contains ints
 input1 = [1,2,3,4,5]
@@ -43,17 +45,21 @@ input3 = [10,9,8,7,6,5,4,3,2,1]
 input4 = [10,9,8,7,6,5,4,3,2,1,0,-1,-2,-3,-4,-5]
 
 print(f"\nmin and max (iterative algorithm): {min_max_iterative(input1)}")
-print(f"min and max (sort algorithm): {min_max_iterative(input1)}")
-print(f"min and max (built-in algorithm): {min_max_iterative(input1)}")
+print(f"min and max (sort algorithm): {min_max_sort(input1)}")
+print(f"min and max (built-in algorithm): {min_max_builtin(input1)}")
+print(f"min and max (priority queue alg): {min_max_priority_queues(input1)}")
 
 print(f"\nmin and max (iterative algorithm): {min_max_iterative(input2)}")
-print(f"min and max (sort algorithm): {min_max_iterative(input2)}")
-print(f"min and max (built-in algorithm): {min_max_iterative(input2)}")
+print(f"min and max (sort algorithm): {min_max_sort(input2)}")
+print(f"min and max (built-in algorithm): {min_max_builtin(input2)}")
+print(f"min and max (priority queue alg): {min_max_priority_queues(input2)}")
 
 print(f"\nmin and max (iterative algorithm): {min_max_iterative(input3)}")
-print(f"min and max (sort algorithm): {min_max_iterative(input3)}")
-print(f"min and max (built-in algorithm): {min_max_iterative(input3)}")
+print(f"min and max (sort algorithm): {min_max_sort(input3)}")
+print(f"min and max (built-in algorithm): {min_max_builtin(input3)}")
+print(f"min and max (priority queue alg): {min_max_priority_queues(input3)}")
 
 print(f"\nmin and max (iterative algorithm): {min_max_iterative(input4)}")
-print(f"min and max (sort algorithm): {min_max_iterative(input4)}")
-print(f"min and max (built-in algorithm): {min_max_iterative(input4)}")
+print(f"min and max (sort algorithm): {min_max_sort(input4)}")
+print(f"min and max (built-in algorithm): {min_max_builtin(input4)}")
+print(f"min and max (priority queue alg): {min_max_priority_queues(input4)}")
