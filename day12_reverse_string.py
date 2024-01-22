b2cf81db1619a118fd738a541eb4d24b756bf3fb
LawrenceLee <>
2024-01-22 09:27:41 -0800

    finished day12 reverse string

---
+++ day12_reverse_string.py
@@ -0,0 +1,54 @@
+'''
+Write a program to reverse a given string.
+'''
+
+'''
+Like all programming problems, there are multiple ways/approaches/algorithsm to solve this problem.
+
+Ideas:
+1. use the Python built-in reverse() function
+    a. this is the shortest code and requires the least amount of effort, but is not accepted in technical interviews
+    b. we also don't know the time and space complexities of this built-in function, unless we look at the source code
+2. iterative approach: use a loop to traverse the string from the end to the beginning
+    a. we can either swap in-place or use a extra temporary array
+3. recursive approach: 
+'''
+
+def reverse_builtin(input_str):
+    # convert string to array/list, reverse the list, then convert back to string
+    return ''.join(reversed(list(input_str)))
+
+def reverse_iterative(input_str):
+    # convert string to array/list
+    input_list = list(input_str)
+
+    # use 2 index pointers, one at the beginning and one at the end
+    start = 0
+    end = len(input_list) - 1
+
+    # swap elements at start and end pointers
+    while start < end:
+        input_list[start], input_list[end] = input_list[end], input_list[start]
+
+        # move pointers to the next indices
+        start += 1
+        end -= 1
+
+    # convert list back to string
+    return ''.join(input_list)
+
+def reverse_recursive(input_str):
+    # base case
+    if len(input_str) == 0:
+        return ''
+
+    # recursive case
+    return reverse_recursive(input_str[1:]) + input_str[0]
+
+
+input1 = "!abcefghijklmnopqrstuvwxyz."
+
+assert reverse_builtin(input1) == reverse_iterative(input1) == reverse_recursive(input1)
+print(reverse_builtin(input1))
+print(reverse_iterative(input1))
+print(reverse_recursive(input1))
